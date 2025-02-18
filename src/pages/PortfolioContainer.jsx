70f5a78e45a5f4db150ff65d79b1f2da04e37b47
Heretiks <julienlarguier1@gmail.com>
2025-02-18 19:14:35 +0100

    Oublie retours du 17/02

--- src/pages/PortfolioContainer.jsx
+++ src/pages/PortfolioContainer.jsx
@@ -52,21 +52,8 @@ function PortfolioContainer() {
         const isHomePage = window.location.pathname === '/';
 
         // Applique les styles si l'URL est '/' et l'appareil est tactile
+        // Test -> ne sais même plus si c'est utile
         if (isTouchDevice && isHomePage) {
-
-            // Test - Leger scroll sur l'accueil mobile
-            window.addEventListener('load', () => {
-                document.body.style.height = '101vh'; // Ajoute une légère marge
-
-                setTimeout(() => {
-                    window.scrollTo(0, 1); // Scroll léger
-                    document.body.style.overflow = 'hidden'; // Bloque ensuite le scroll
-                }, 100);
-
-                console.log(scrollY);
-            });
-
-
             document.body.style.overflow = 'hidden';
             document.body.style.position = 'relative';
         }
